7aac691441897e8cdf521bec7e97679d215b2401
kowalej <kowaleskij@gmail.com>
2020-04-29 20:06:12 -0400

    Converted to .NET Core console application.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using CommandLine;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Diagnostics;
